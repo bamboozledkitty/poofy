f5a93b27f3136ecf33dfc46fbf496f0ae7f992d2
bamboozledkitty <keith.klv@gmail.com>
2024-11-16 00:10:19 +0530

    Initial Commit

---
+++ Poofy/PoofyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoofyApp.swift
+//  Poofy
+//
+//  Created by Keith Vaz on 16/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PoofyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PoofyTests/PoofyTests.swift
@@ -0,0 +1,16 @@
+//
+//  PoofyTests.swift
+//  PoofyTests
+//
+//  Created by Keith Vaz on 16/11/24.
+//
+
+import Testing
+
+struct PoofyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
